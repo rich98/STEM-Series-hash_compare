ad79d0b12a3a47a400e3a5118a8e13aa51ce9146
rich98 <51236157+rich98@users.noreply.github.com>
2024-01-14 09:48:37 +0000

    Update hashcompare.bat

--- hashcompare.bat
+++ hashcompare.bat
@@ -1,20 +1,20 @@
 @ECHO OFF
 rem ********************************
 rem *****  HASH MATCH CHECKER  *****
-rem ***** By Richard Wadsworth *****
+rem ***** By Rich98            *****
 Rem ********************************
 
 rem get the hash and set the varible for file1
 
 set "HHFILE1=" & for /F "skip=1 delims=" %%H in ('
-    2^> nul CertUtil -hashfile "VMware-workstation-full-17.5.0-22583795(dec).exe" SHA256
+    2^> nul CertUtil -hashfile "<add path and filename>" SHA256
 ') do if not defined HFILE1 set "HFILE1=%%H"
 
 
 rem get the has and set the varible for file 2
 
 set "HFILE2=" & for /F "skip=1 delims=" %%H in ('
-    2^> nul CertUtil -hashfile "VMware-workstation-full-17.5.0-22583795.exe" SHA256
+    2^> nul CertUtil -hashfile "<add path and filename>" SHA256
 ') do if not defined HFILE2 set "HFILE2=%%H"
 
 rem /* compare the hashes; regard that you need surrounding `%%` to read variables;
