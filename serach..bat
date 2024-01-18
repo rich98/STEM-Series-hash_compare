779e086057d7d26e3c80f33b25ba45f6596ff2dc
rich98 <51236157+rich98@users.noreply.github.com>
2024-01-18 06:27:21 +0000

    Update serach..bat

--- serach..bat
+++ serach..bat
@@ -1,18 +1,14 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set "search_file1=java.txt"
-set "search_file2=javaw.exe"
+set "search_file1=file1.txt"
+set "search_file2=file2.txt"
 set "results_file=results.txt"
 
 rem Delete the results file if it exists
 if exist %results_file% del %results_file%
 
-@echo off
-
-rem for %%d in (A: B: C: D: E: F: G: H: I: J: K: L: M: N: O: P: Q: R: S: T: U: V: W: X: Y: Z:) do (
-for %%d in (A: B: C: D:) do (
-
+for %%d in (A: B: C: D: E: F: G: H: I: J: K: L: M: N: O: P: Q: R: S: T: U: V: W: X: Y: Z:) do (
     if exist "%%d\" (
         echo Searching in %%d\
         call :search "%%d\" %search_file1%
@@ -24,16 +20,15 @@ echo Search complete. Results saved to %results_file%
 pause
 exit /b
 
-@echo off
-
 :search
 for /r %1 %%f in (%2) do (
     echo Found: %%f >> %results_file%
     
-    rem Display file properties using WMIC
+    rem Display only "Name" and "Version" properties using WMIC
     set "filepath=%%f"
-    for /f "delims=" %%p in ('wmic datafile where "name='!filepath:\=\\!'" get /format:list ^| find "="') do (
-        echo File Property: %%p >> %results_file%
+    for /f "tokens=1,* delims==" %%p in ('wmic datafile where "name='!filepath:\=\\!'" get /format:list ^| find "="') do (
+        if "%%p"=="Name" echo File Property: %%q >> %results_file%
+        if "%%p"=="Version" echo File Property: %%q >> %results_file%
     )
 )
 exit /b
